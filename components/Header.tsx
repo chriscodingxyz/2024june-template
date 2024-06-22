54905c7371cffea1d6d3eea7c6f1cbfca470e4db
cherrydub <cherrydub@gmail.com>
2024-06-22 10:29:16 +0100

    added avatar and punks in public images

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { ModeToggle } from "@/components/ModeToggle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Header() {
   return (
-    <header className="">
+    <header className="flex items-center justify-between">
+      <Avatar>
+        <AvatarImage src="/punk1534.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
       <ModeToggle />
     </header>
   );
